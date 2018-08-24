22885d72eee952b7fb59352fe4aa105869e49ef9
Sasha Varlamov <sasha@sashavarlamov.com>
2018-08-24 15:40:11 +0800

    Use a single browser instance, sanitize page

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ const app = express();
 const puppeteer = require('puppeteer');
 
 const port = 3000;
+
+const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--disable-dev-shm-usage']
+});
+
+const sanitizePage = () => {
+    const elements = document.querySelectorAll('script, link[rel=import]');
+    for (const e of Array.from(elements)) {
+        e.remove();
+    }
+}
+
 app.get('*', async (req, res) => {
     try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--disable-dev-shm-usage']
-        });
         const page = await browser.newPage();
-        
         await page.setUserAgent('bot-exlpre-0.0.1');
-
+        
         await page.goto(req.originalUrl.substring(1), {
             waitUntil: "networkidle0",
         });
+        
+        await page.evaluate(sanitizePage)
 
         const html = await page.evaluate(() => {
             return document.documentElement.innerHTML;
         });
 
         res.send(html);
-
     } catch(e) {
         console.log(e);
         res.status(500).send("ERROR");
